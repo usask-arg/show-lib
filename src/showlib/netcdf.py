48d13416dfca7ffd9be727216d4fd2de732dee1c
Daniel Zawada <daniel.zawada@usask.ca>
2024-01-25 14:08:22 -0600

    Add something

---
+++ src/showlib/netcdf.py
@@ -0,0 +1,20 @@
+from __future__ import annotations
+
+from pathlib import Path
+
+
+class GranuledNetCDF:
+    def __init__(self) -> None:
+        pass
+
+    @classmethod
+    def load_from_yaml(cls, file: Path) -> GranuledNetCDF:
+        pass
+
+    @classmethod
+    def load(cls, file: Path) -> GranuledNetCDF:
+        pass
+
+    @classmethod
+    def save(cls, file: Path) -> GranuledNetCDF:
+        pass
