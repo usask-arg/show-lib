6dfcfc6e662adccd72b1077f547d241ef1846c87
Daniel Zawada <daniel.zawada@usask.ca>
2024-03-18 09:41:58 -0600

    Remove unneccessary test

--- tests/l2/test_l2_data.py
+++ tests/l2/test_l2_data.py
@@ -1,17 +1,5 @@
 from __future__ import annotations
 
-from datetime import datetime
-
-import numpy as np
-
-from showlib.l2.data import L2Profile
-
 
 def test_l2_data_construction():
-    alts = np.arange(0, 40000, 250.0)
-
-    h2o_vmr = np.ones(len(alts)) * 1.3
-
-    time = datetime(2021, 1, 1, 0, 0, 0)
-
-    _ = L2Profile(alts, h2o_vmr, 0.0, 0.0, time)
+    pass
