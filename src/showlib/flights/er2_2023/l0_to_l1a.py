e687fc8bc46a2f0d2c2fb7c0e7d4036b3162d3fe
Jeffery Adam Langille <t383r@unb.ca>
2024-04-01 14:18:55 -0300

    moved l0_to_l1A to er2_2023

--- src/showlib/flights/er2_2023/l0_to_l1a.py
+++ src/showlib/flights/er2_2023/l0_to_l1a.py
@@ -6,10 +6,10 @@ import numpy as np
 import xarray as xr
 from skretrieval.util import configure_log
 
-from showlib.flights.er2_2023.Platform.ER2Platform import ER2Platform as er2
-from showlib.flights.er2_2023.Specifications.shower2_specs import SHOW_specs as specs
 from showlib.flights.er2_2023.l1a.data import L1AFileWriter, L1AImage
 from showlib.flights.er2_2023.l1a.l1a_processing import level1A_processing as l1a
+from showlib.flights.er2_2023.Platform.ER2Platform import ER2Platform as er2
+from showlib.flights.er2_2023.Specifications.shower2_specs import SHOW_specs as specs
 
 
 def er2_attitude(show_utc, filename):
