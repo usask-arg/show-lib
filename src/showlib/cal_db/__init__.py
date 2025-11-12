9dbad80e1561b5dd22e1967546afefa99ee44bbc
Daniel Zawada <daniel.zawada@usask.ca>
2025-11-12 10:41:41 -0600

    bump

--- src/showlib/cal_db/__init__.py
+++ src/showlib/cal_db/__init__.py
@@ -1,5 +1,7 @@
-import xarray as xr
+from __future__ import annotations
+
 import numpy as np
+import xarray as xr
 
 
 class CalibrationDatabase:
@@ -12,27 +14,5 @@ class CalibrationDatabase:
     def dark_current(self, time, detector_temperature) -> np.ndarray:
         pass
 
-    def nominal_central_wavenumbers(self, time) -> np.ndarray:
+    def nominal_central_wavenumbers(self, time) -> np.ndarray:  # noqa: ARG002
         return self._ds["sample_wavenumber"].to_numpy()
-
-
-    @classmethod
-    def from_np_arrays(
-
-    ):
-        ds = xr.Dataset()
-
-        ds["filter_shape"] = filter_shape
-        ds["pixel_response"] = pixel_response
-        ds["abs_cal"] = abs_cal
-        ds["wavenumbers"] = wavenumbers
-        ds["bad_pixel_map"] = xr.DataArray(
-            bad_pixel_map, dims=["pixelheight", "pixelcolumn"]
-        )
-        ds["wavenumber_spacing"] = wavenumber_spacing
-        ds["Littrow"] = Littrow
-        ds["ThetaL"] = ThetaL
-        ds["opd_x"] = opd_x
-        ds["pos_x"] = pos_x
-        ds["pos_y"] = pos_y
-        ds["zpd"] = zpd
